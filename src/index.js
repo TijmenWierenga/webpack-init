7aad9717dd9f834adaad5ce7eac271d2d57201b2
Maarten Bicknese <maarten.bicknese@gmail.com>
2018-04-30 17:09:46 +0200

    Optimize Webpack usage
     - Use Webpack's mode instead of separate files
     - Use Webpack's sane defaults
     - Move index.html to the source files
     - Remove extract-text-plugin as it is not compatible with Webpack 4
     - Add more source code to demo Webpack features
     - Compress SASS output
     - Add sourcemaps to (S)CSS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import './styles/app.scss';
+const foo = require('./foo');
 
-console.log("This is a test");
+if (module.hot) {
+    module.hot.accept('./foo', () => {
+        cheers = require('./foo') - 1;
+        updateCheers();
+    });
+}
+
+let cheers = foo;
+const state = document.getElementById('state');
+
+const updateCheers = () =>
+    state.textContent = state.textContent.replace(/(\d+)/, ++cheers);
+
+
+state.textContent = `Running ${process.env.NODE_ENV} build. Cheered: ${foo} times.`;
+document.getElementById('cheer').onclick = updateCheers;
